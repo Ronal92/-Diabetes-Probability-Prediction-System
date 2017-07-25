946b517d567fe81d4a741307046cd2f15c55f4d2
Ronal92 <21000189@handong.edu>
2017-07-25 14:23:24 +0900

    Making login

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 
 from flask import Flask, render_template, request, redirect, flash, url_for, session
-# from flaskext.mysql import MySQL
+from flaskext.mysql import MySQL
 import csv, math
 import json
 
@@ -18,18 +18,21 @@ hyperList1 = []
 hyperList2 = []
 calWeightList = []
 
+isManager =''
+
+
 app = Flask(__name__)
 app.secret_key = 'secret'
 
 
-# mysql = MySQL()
-# '''
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
-# app.config['MYSQL_DATABASE_DB'] = 'test'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
-# '''
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+app.config['MYSQL_DATABASE_DB'] = 'test'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
 
 def calculateResult():
 	i =0
@@ -103,10 +106,43 @@ readCSVweight() # Read csv before app start
 def showLoginPage():
 	return render_template("login.html")
 
+#로그인 화면 처리
+@app.route("/", methods=['POST'])
+def processLogin():
+
+	global isManager
+
+
+	name=request.form['id']
+	con=mysql.connect()
+	cursor =con.cursor()
+
+	## 검색
+	cursor.execute("SELECT * FROM my_user WHERE name='"+request.form['id']+"' AND pwd='"+request.form['pwd']+"';")
+	data = cursor.fetchone()
+	if data is None:
+		return redirect('/')
+		
+	else:
+
+		if data[3] == 'manager' : ## 관리자인지 구분한다 ##
+			isManager = 'true'
+			flash('관리자')
+		else :
+			isManager = 'false'
+
+		return render_template("main.html", cur_name = name);
+
+
+
 # 메인 화면
 @app.route("/main")
 def showMainPage():
-	return render_template("hello.html", cur_name = request.args.get('cur_name'))
+	global isManager
+	if isManager == 'true' :
+		flash('관리자')
+
+	return render_template("main.html")
 
 ''' # TODO : 로긴처리
 def tryLogin():
@@ -131,29 +167,26 @@ def showSignUp():
 # 사용자 등록 요청
 @app.route("/signUps", methods=['POST'])
 def saveSignUp():
-	name=request.form['inputName']
-	email=request.form['inputEmail']
+	name=request.form['inputID']
+	pwd=request.form['inputPWD']
 
-	# con=mysql.connect()
-	# cursor =con.cursor()
+	con=mysql.connect()
+	cursor =con.cursor()
 
-	# ## 검색
-	# cursor.execute("SELECT * FROM my_user WHERE name='"+name+"' AND email='"+email+"';")
-	# data = cursor.fetchone()
-	# if data is None:
-	# 	query="INSERT INTO my_user(name, email) VALUES ('" + name +"','" + email +"');"
-	# 	cursor.execute(query)
-	# 	con.commit()
-	# 	flash('사용자가 등록되었습니다.')
+	## 검색
+	cursor.execute("SELECT * FROM my_user WHERE name='"+name+"' AND pwd='"+pwd+"';")
+	data = cursor.fetchone()
+	if data is None:
+		query="INSERT INTO my_user(name, pwd) VALUES ('" + name +"','" + pwd +"');"
+		cursor.execute(query)
+		con.commit()
+		return redirect(url_for('showMainPage'))
+
 		
-	# else:
-	# 	flash('같은 사용자가 존재합니다.')
+	else:
+		print ('이미 존재')
+		return render_template("signup.html", isUser='true')
 
-
-	if name=='koreauniv' : ## 관리자인지 구분한다 ##
-		flash('관리자')
-
-	return redirect(url_for('showMainPage',cur_name=name))
 
 # 계산 실행 
 @app.route("/measure", methods=['GET'])
@@ -191,6 +224,11 @@ def measureDiabetsResult():
 	print(calWeightList)
 	calculateResult()
 	return render_template('result.html') #, result = calculateResult() )
+
+# 계산 모델
+@app.route("/setting")
+def valueSet():
+	return render_template('setting.html')
 
 
 if __name__=="__main__":
